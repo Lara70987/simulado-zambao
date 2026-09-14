2b6798d4ba8db46f4c4375a22985952cef063b62
Lara <lara.abduni@gmail.com>
2026-09-14 18:21:37 -0300

    de mal a pior

--- src/main/java/com/socorro/simulado/entity/Curso.java
+++ src/main/java/com/socorro/simulado/entity/Curso.java
@@ -1,14 +1,8 @@
 package com.socorro.simulado.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Curso {
 
     @Id
@@ -21,4 +15,50 @@ public class Curso {
     private String descricao;
 
     private boolean deleted = false;
+
+    public Curso() {
+    }
+
+    public Curso(Long id, String nome, String descricao, boolean deleted) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.deleted = deleted;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }
